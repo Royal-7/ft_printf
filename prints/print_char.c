111a232b1444a3d07557532c25f808be2acba72a
Royal-7 <n.r.bao11@gmail.com>
2018-12-22 15:58:25 -0800

    Add files via upload

--- prints/print_char.c
+++ prints/print_char.c
@@ -6,27 +6,50 @@
 /*   By: abao <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 19:32:54 by abao              #+#    #+#             */
-/*   Updated: 2018/12/17 19:19:39 by abao             ###   ########.fr       */
+/*   Updated: 2018/12/18 18:42:17 by abao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+/*
+* For char conversions, the flags +, space, and precision don't have any effect.
+*/
 t_flags print_char(const char *format, va_list args, t_flags list)
 {
-	char			a;
-	char			*b;
-	signed char		c;
-	unsigned char	d;
-	signed char		*e;
+	int		a;
 
-	if (list.flag.minus == 1)
+	if (list.min != 0)
 	{
-		if (list.flag.
+		if (list.flags.minus == 1)
+		{
+			if (list.zero == 1)
+			{
+				while (list.min > 1)
+				{
+					ft_putchar('0');
+					list.min--;
+				}
+			}
+			else
+			{
+				while (list.min > 1)
+				{
+					ft_putchar(' ');
+					list.min--;
+				}
+			}
+		}
+		else
+		{
+			ft_putchar(va_arg(args, char));
+			list.length = 1;
+		}
 	}
 	else
 	{
-	
+		ft_putchar(va_arg(args, char));
+		list.length = 1;
 	}
 	else
 		list.correct = 0;
